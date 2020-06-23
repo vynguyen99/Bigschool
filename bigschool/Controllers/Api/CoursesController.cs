dff7d5bd9ebf5f1ed802609b7f23f5a76e19c341
vynguyen99 <vynguyen112299@gmail.com>
2020-06-23 09:11:23 +0700

    commit

--- bigschool/Controllers/Api/CoursesController.cs
+++ bigschool/Controllers/Api/CoursesController.cs
@@ -3,28 +3,31 @@ using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace bigschool.Controllers.Api
 {
-    public class CoursesController: ApiController
+    public class CoursesController : ApiController
     {
-        public ApplicationDbContext _dbContext { get; set; }
-        public CoursesController()
-        {
-            _dbContext = new ApplicationDbContext();
-        }
-        [HttpDelete]
-        public IHttpActionResult Cancel (int id)
-        {
-            var userId = User.Identity.GetUserId();
-            var course = _dbContext.Course.Single(c => c.id == id && c.LectuterId == userId);
-            if (course.IsCanceled)
-                return NotFound();
-            course.IsCanceled = true;
-            _dbContext.SaveChanges();
-            return Ok();
-        }
+        
+            public ApplicationDbContext _dbContext { get; set; }
+            public CoursesController()
+            {
+                _dbContext = new ApplicationDbContext();
+            }
+            [HttpDelete]
+            public IHttpActionResult Cancel(int id)
+            {
+                var userId = User.Identity.GetUserId();
+                var course = _dbContext.Course.Single(c => c.id == id && c.LectuterId == userId);
+                if (course.IsCanceled)
+                    return NotFound();
+                course.IsCanceled = true;
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+        
     }
 }
